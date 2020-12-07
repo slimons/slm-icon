7d5e69b3a6430f14d26dd7bf8d97d8aacd15c25c
jponferrada26@gmail.com <jponferrada@nosolosoftware.es>
2020-12-07 22:24:01 +0100

    [master] Added functionality for simple use

--- example/components/Simple.vue
+++ example/components/Simple.vue
@@ -1,9 +1,21 @@
 <template lang="pug">
-slm-input
+slm-input(v-model="valueInput")
+p {{valueInput}}
 </template>
 
 <script>
+import {reactive, toRefs} from 'vue'
+
 export default {
+  setup() {
+    const data = reactive({
+      valueInput: ''
+    })
+
+    return {
+      ...toRefs(data)
+    }
+  }
 }
 </script>
 
--- src/SlmInput.vue
+++ src/SlmInput.vue
@@ -1,10 +1,37 @@
 <template lang="pug">
 #slm-input
+  input(v-model="currentValue")
   p Esto es un SL
 </template>
 
 <script>
+import {reactive, watch, toRefs} from 'vue'
+
 export default {
-  name: 'SlmInput'
+  name: 'SlmInput',
+  props: {
+    modelValue: {type: [String, Number]}
+  },
+  setup(props, context) {
+    // --- data ---
+    const data = reactive({
+      currentValue: ''
+    })
+
+    // --- use currentValue ---
+    watch(() => props.modelValue, async newVal => {
+      if (!props.modelValue) return
+
+      data.currentValue = newVal
+    }, {immediate: true})
+
+    watch(() => data.currentValue, async newVal => {
+      context.emit('update:modelValue', newVal)
+    })
+
+    return {
+      ...toRefs(data)
+    }
+  }
 }
 </script>
